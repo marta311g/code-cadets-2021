f06765c1e1cc1f5e71b2b34bae760f988b1bd017
Marta Gračner <marta.gracner@fer.hr>
2021-05-12 15:38:14 +0200

    added another version of fizzbuzz

--- homework_1/fizzBuzz.go
+++ homework_1/fizzBuzz.go
@@ -1,10 +1,31 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func main() {
 	start := 10
 	end := 20
+	fizzbuzzOutputList := []string{}
 
 
+	// version 1: with array - saving elements then printing
+	for i := start; i <= end; i++ {
+		if i % 3 == 0 && i % 5 == 0 {
+			fizzbuzzOutputList = append(fizzbuzzOutputList, "FizzBuzz")
+		} else if  i % 3 == 0 {
+			fizzbuzzOutputList = append(fizzbuzzOutputList, "Fizz")
+		} else if i % 5 == 0 {
+			fizzbuzzOutputList = append(fizzbuzzOutputList, "Buzz")
+		} else {
+			fizzbuzzOutputList = append(fizzbuzzOutputList, strconv.Itoa(i))
+		}
+	}
+	println(strings.Join(fizzbuzzOutputList, " "))
+
+	// version 2: without array - printing elements
 	for i := start; i <= end; i++ {
 		if i % 3 == 0 && i % 5 == 0 {
 			print("FizzBuzz ")
