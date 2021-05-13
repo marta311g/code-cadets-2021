dab276a62c3d7360e8ae763651505105e372c482
Marta Gračner <marta.gracner@fer.hr>
2021-05-13 14:20:13 +0200

    brackets are now configurable

--- homework_1/progressiveTax.go
+++ homework_1/progressiveTax.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
 )
 
 type taxBracket struct {
@@ -27,18 +34,53 @@ func calculateTax(inputValue float64, taxBrackets []taxBracket) float64 {
 	return tax
 }
 
-func addTaxBracket(lowerLimit, upperLimit, percentage float64) taxBracket {
+func makeTaxBracket(lineElements []string) taxBracket {
+	lowerLimit, err := strconv.ParseFloat(lineElements[0], 64)
+	if err != nil {
+		log.Fatal( errors.WithMessage(err, "invalid lower limit"), )
+	}
+	upperLimit, err := strconv.ParseFloat(lineElements[1], 64)
+	if err != nil {
+		log.Fatal( errors.WithMessage(err, "invalid lower limit"), )
+	}
+	percentage, err := strconv.ParseFloat(lineElements[2], 64)
+	if err != nil {
+		log.Fatal( errors.WithMessage(err, "invalid lower limit"), )
+	}
 	return taxBracket{ lowerLimit, upperLimit, percentage}
 }
 
-func main() {
-	inputValue := 7000.0
-
+func getTaxBrackets() []taxBracket {
 	var taxBrackets []taxBracket
-	taxBrackets = append(taxBrackets, addTaxBracket(0, 1000, 0))
-	taxBrackets = append(taxBrackets, addTaxBracket(1000, 5000, 0.1))
-	taxBrackets = append(taxBrackets, addTaxBracket(5000, 10000, 0.2))
-	taxBrackets = append(taxBrackets, addTaxBracket(10000, -1, 0.3))
+
+	f, err := os.Open("./homework_1/brackets.txt")
+	if err != nil {
+		log.Fatal( errors.WithMessage(err, "error while opening a file"), )
+	}
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	var text []string
+
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+
+	defer f.Close()
+
+	for _, line := range text {
+		taxBrackets = append(taxBrackets, makeTaxBracket(strings.Split(line, ";")))
+	}
+	return taxBrackets
+}
+
+func main() {
+	var inputValue float64
+	fmt.Print("Please, enter input value: ")
+	fmt.Scanf("%f", &inputValue)
+
+	var taxBrackets = getTaxBrackets()
 
 	fmt.Printf("Za ulaznu vrijednost %.2f iznos poreza je: %.2f\n", inputValue, calculateTax(inputValue, taxBrackets))
 }
