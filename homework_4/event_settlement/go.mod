75b3d488d202248b4d402b2e8dcdd922a54d439b
Marta Gračner <marta.gracner@fer.hr>
2021-06-03 15:29:14 +0200

    implemented task 3

---
+++ homework_4/event_settlement/go.mod
@@ -0,0 +1,8 @@
+module github.com/superbet-group/code-cadets-2021/lecture_4/01_event_api
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/pkg/errors v0.9.1
+)
---
+++ homework_4/event_settlement/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+)
+
+const activeBetsURL = "/bets?status=active"
+const eventUpdateURL = "/event/update"
+
+func getActiveEvents() ([]string, error) {
+	httpResponse, err := http.Get(activeBetsURL)
+	if err != nil {
+		return nil, err
+	}
+
+	bodyContent, err := ioutil.ReadAll(httpResponse.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var decodedBets []bet
+	err = json.Unmarshal(bodyContent, &decodedBets)
+	if err != nil {
+		return nil, err
+	}
+
+	var eventIds []string
+	for _, bet := range decodedBets {
+		eventIds = append(eventIds, bet.selectionId)
+	}
+
+	return eventIds, nil
+}
+
+func postEventUpdates(eventIds []string) error {
+	status := []string{"won", "lost"}
+	for _, eventId := range eventIds {
+
+		eventJson, err := json.Marshal(eventUpdate{
+			id: eventId,
+			outcome: status[rand.Intn(2)],
+		})
+
+		_, err = http.Post(eventUpdateURL, "application/json", bytes.NewBuffer(eventJson))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	eventIds, err := getActiveEvents()
+	if err != nil {
+		log.Fatal(errors.WithMessage(err, "failed to get active bets"))
+	}
+
+	err = postEventUpdates(eventIds)
+	if err != nil {
+		log.Fatal(errors.WithMessage(err, "failed to post event updates"))
+	}
+}
+
+type bet struct {
+	id                   string  `json:"id"`
+	customerId           string  `json:"customer_id"`
+	selectionId          string  `json:"selection_id"`
+	selectionCoefficient float64 `json:"selection_coefficient"`
+	payment              float64 `json:"payment"`
+	payout               float64 `json:"payout"`
+}
+
+type eventUpdate struct {
+	id      string `json:"id"`
+	outcome string `json:"outcome"`
+}
